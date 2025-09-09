1c6342faae5e6dd1b20c99cbc6bc8e898281c6ca
Chauck Corgan <spacedrive22@gmail.com>
2025-09-09 12:18:46 +0000

    eu até gostei da ideia, mas eu achei muito simples, queria algo mais atu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -88,10 +88,15 @@ export default {
             height: '0',
           },
         },
+        'fade-in-out': {
+          '0%, 100%': { opacity: '0', transform: 'scale(0.9)' },
+          '50%': { opacity: '1', transform: 'scale(1)' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'fade-in-out': 'fade-in-out 4s ease-in-out infinite',
       },
     },
   },
