015154eb7b40a45001ca4960c684201c5a6eb311
Chauck Corgan <spacedrive22@gmail.com>
2025-09-23 20:06:41 +0000

    Try fixing this error: `Console Error: `DialogContent` requires a `Dialo

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -11,7 +11,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetTitle } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -207,6 +207,9 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
+            <div className="sr-only">
+              <SheetTitle>Filtros</SheetTitle>
+            </div>
             <div className="flex h-full w-full flex-col">{children}</div>
           </SheetContent>
         </Sheet>
@@ -764,5 +767,7 @@ export {
   SidebarTrigger,
   useSidebar,
 }
+
+    
 
     
