b08ad741ef742d20e5cd08b42226b43213a68954
Chauck Corgan <spacedrive22@gmail.com>
2025-09-30 19:32:26 +0000

    vou colar aqui veja se consegue me ajudar:
    <svg id="Compass-90" xmlns="

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,27 +1,39 @@
 import { cn } from "@/lib/utils";
 
-// ATENÇÃO: O conteúdo deste SVG é um placeholder.
-// Por favor, copie o conteúdo do seu arquivo 'logo-frotaai.svg' e cole aqui dentro,
-// substituindo este SVG de exemplo.
 const Logo = ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
   <svg
+    id="Compass-90"
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    version="1.1"
+    viewBox="0 0 99 106.2"
     className={cn("text-primary", className)}
     {...props}
   >
-    <path d="M14 16.5V14a2 2 0 0 0-2-2h-1a2 2 0 0 0-2 2v2.5" />
-    <path d="M14 14.5V12a2 2 0 0 0-2-2h-1a2 2 0 0 0-2 2v2.5" />
-    <path d="M20 10h-2a2 2 0 0 0-2 2v6H8a2 2 0 0 1-2-2v-3.5a2 2 0 0 1 2-2h3" />
-    <path d="M19 8h-2.1a2 2 0 0 0-1.9.9l-2.1 3.2c-.2.2-.4.4-.7.5" />
-    <path d="M10 6H9a2 2 0 0 0-2 2v3.5a2 2 0 0 0 2 2h2" />
-    <circle cx="7" cy="17" r="2" />
-    <circle cx="17" cy="17" r="2" />
+    {/* Generator: Adobe Illustrator 29.8.1, SVG Export Plug-In . SVG Version: 2.1.1 Build 2)  */}
+    <defs>
+      <style>
+        {`
+          .st0, .st1 {
+            fill: #fff;
+          }
+          .st2 {
+            fill: #2a62ff;
+          }
+          .st1 {
+            stroke: #2a62ff;
+            stroke-miterlimit: 10;
+            stroke-width: 3.8px;
+          }
+        `}
+      </style>
+    </defs>
+    <path className="st2" d="M85.7,66.1c-7.5,12.5-17.1,14.5-25.4,23.1-4,4.1-9.9,24.7-15.1,13.9-8.6-17.8-8-12.6-21.5-24.4C-5.4,53.3,3.6,5.7,42.4,.4c36.9-5,62.9,33.1,43.3,65.6Z"/>
+    <circle className="st0" cx="49.5" cy="43.6" r="35.6"/>
+    <g>
+      <path className="st1" d="M48.8,53.9c4.1.4,7.8-2.7,8.2-6.8.4-4.1-2.7-7.8-6.8-8.2-4.1-.4-7.8,2.7-8.2,6.8-.4,4.1,2.7,7.8,6.8,8.2Z"/>
+      <path className="st1" d="M4.7,86l57.1-25.1c.3-.2.6-.4.9-.7L96.8,9.8c1.1-1.7-.6-3.8-2.5-3l-57.1,25.1c-.3.2-.6.4-.9.7L2.2,83.1c-1.1,1.7.6,3.8,2.5,3ZM50.2,38.9c4.1.4,7.2,4.1,6.8,8.2-.4,4.1-4.1,7.2-8.2,6.8-4.1-.4-7.2-4.1-6.8-8.2.4-4.1,4.1-7.2,8.2-6.8Z"/>
+    </g>
+    <circle className="st2" cx="49.5" cy="46.4" r="7.5"/>
   </svg>
 );
 
